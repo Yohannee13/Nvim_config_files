3a2c229be1d99b0df2d8c3222b4c53634e3f5b80
yoandria <yoandria@student.42antananarivo.mg>
2026-06-24 06:30:33 +0300

    Fix: fixed the expand of tab for python files

--- lua/after/ftplugin/python.lua
+++ lua/after/ftplugin/python.lua
@@ -3,7 +3,6 @@ vim.opt_local.textwidth = 79
 
 vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
-vim.opt_local.expandtab = true
+vim.opt_local.expandtab = false
 
 vim.opt_local.formatoptions:append("t")
-
